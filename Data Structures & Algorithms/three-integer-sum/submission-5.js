3fa7d0fdf127bc52ec7c58f550259c2324653a3b
Nicolas Salleron <nicolas.salleron@gmail.com>
2026-03-31 18:53:34 +0200

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const sorted = nums.sort((a, b) => a - b)
+        const res = []
+        for(let i = 0; i < sorted.length - 2; i++){
+            if (sorted[i] === sorted[i - 1]) continue;
+            let j = i + 1
+            let k = sorted.length - 1
+            while(j < k){
+                let sum = sorted[i] + sorted[j] + sorted[k];
+                if(sum === 0){
+                    res.push([sorted[i], sorted[j], sorted[k]])
+                    while (j < k && sorted[j] === sorted[j + 1]) j++; //duplicates
+                    while (j < k && sorted[k] === sorted[k - 1]) k--; //duplicates
+                    j++
+                    k--
+                } else if (sum < 0) {
+                    j++
+                } else {
+                    k--
+                }
+            }
+        }
+        return res
+    }
+}
