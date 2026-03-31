ca66cbe7b83f7ff101222de263c95bafe4253cef
Nicolas Salleron <nicolas.salleron@gmail.com>
2026-03-31 17:33:47 +0200

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let l = 0
+        let r = numbers.length - 1 
+        while( l < r){
+            if(numbers[l] + numbers[r] > target){
+                r--
+            } else if (numbers[l] + numbers[r] < target){
+                l++
+            } else {
+                console.log(`found ${l} => ${numbers[l]} | ${r} => ${numbers[r]}`)
+                return [l + 1 , r + 1]
+
+            }
+        }
+        return false 
+    }
+}
