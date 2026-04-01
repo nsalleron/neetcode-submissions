2ef70d2b3dbc988e3269815fd004f3f9f3f90a05
Nicolas Salleron <nicolas.salleron@gmail.com>
2026-04-01 11:13:51 +0200

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    rotate(nums, k) {
+       const n = nums.length
+       k %= n
+
+       const reverse = (l, r) => {
+            while(l < r) {
+                [nums[l], nums[r]] = [nums[r], nums[l]]
+                l++
+                r--
+            }
+       }
+
+       reverse(0, n - 1)
+       reverse(0, k - 1)
+       reverse(k, n - 1)
+    }
+}
