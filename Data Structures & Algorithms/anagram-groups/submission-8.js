362e2dd7d088594955ffa4819d7ae161f95692a4
Nicolas Salleron <nicolas.salleron@gmail.com>
2026-04-21 10:02:26 +0200

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map()
+        const aCharValue = 'a'.charCodeAt(0)
+
+        for(let s of strs) {
+            const freqArray = Array.from({length: 26}, () => 0)
+            for(let c of s){
+                const key = c.charCodeAt(0) - aCharValue
+                freqArray[key] += 1
+            }
+            const key = freqArray.join(',')
+            map.set(key, [...(map.get(key) ?? []), s])
+        }
+
+        return Array.from(map.values())
+
+    }
+}
